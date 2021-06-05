0f859627eb3159826f2cf103fdc5735d9e6187a3
nickr <nicholaspriegel@gmail.com>
2021-06-05 23:20:38 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 
 import drumArray from './Assets/drumLists'
-import DrumBox from './Components/DrumBox'
-import Display from './Components/Display'
+// import DrumBox from './Components/DrumBox'
+// import Display from './Components/Display'
 
 const App = () => {
 
@@ -13,23 +13,44 @@ const App = () => {
     return document.removeEventListener('keydown', e=>playSound(e))
   }, [])
 
+  useEffect(() => {
+    setTimeout(() => setNote0(null), 1000);
+  }, [note0])
+  
   const playSound = (x) => {
     const note1 = drumArray.find(y => y.keyCode === x.keyCode)
     if (note1) {
       setNote0(note1)
-      const sound0 = new Audio(note1.url)
+      const sound0 = document.getElementById(note1.keyTrigger)
       sound0.play()
     }
+  }
+
+  const playSound0 = (x) => {
+    const note2 = document.getElementById(x.keyTrigger)
+    note2.play()
+    setNote0(x)
   }
 
   return (
     <div className="App">
       <div id="drum-machine">
-        <Display note0={note0}/>
-        <DrumBox
-          drumArray={drumArray}
-          playSound={playSound}
-        />
+        <div id="display">
+          <p>{note0 && note0.id}</p>
+        </div>
+        <div id="drum-box">
+          {drumArray.map(x => (
+            <div
+              className="drum"
+              key={x.id}
+              onClick={()=>playSound0(x)}
+            >{x.keyTrigger}
+              <audio className="clip" id={x.keyTrigger}>
+                <source src={x.url}></source>
+              </audio>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
